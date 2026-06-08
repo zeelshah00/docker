e2be198b3fe54f463eb52b450d0a998bac4e288a
zeelshah00 <86899131+zeelshah00@users.noreply.github.com>
2026-06-08 20:32:53 +0000

    June 8 Progress

---
+++ pipeline/pipeline.py
@@ -0,0 +1,17 @@
+
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f"Running pipeline for day {day}")
+
+
+
